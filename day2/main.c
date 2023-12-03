ef2735cf2de4c5907d4be8655bfeb2c5feeb7d32
m.kindritskiy <m.kindritskiy@smartweb.com.ua>
2023-12-04 00:35:31 +0200

    [day2][task2]

--- day2/main.c
+++ day2/main.c
@@ -1,7 +1,12 @@
+#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define MAX_RED 12
+#define MAX_BLUE 14
+#define MAX_GREEN 13
 
 FILE* read_input(char* filename) {
     FILE* file = fopen(filename, "r");
@@ -13,51 +18,64 @@ FILE* read_input(char* filename) {
     return file;
 }
 
-#define MAX_RED 12
-#define MAX_BLUE 14
-#define MAX_GREEN 13
-
 bool ends_with(char* str, char tok) {
     return str[strlen(str)-1] == tok;
 }
 
+int parse_game_num(char* token) {
+    token = strtok(NULL, " ");
+    return atoi(token);
+}
+
+struct GameIteration {
+    int amount;
+    char *color;
+};
+
+typedef struct GameIteration GameIteration;
+
+GameIteration parse_game_iteration(char* token) {
+    int amount = atoi(token);
+    char *color = strtok(NULL, " ");
+
+    if (ends_with(color, ',')
+        || ends_with(color, ';')
+        || ends_with(color, '\n')
+        ) {
+        color[strlen(color)-1] = '\0';
+    }
+
+    GameIteration game = {
+        .color = color,
+        .amount = amount
+    };
+    return game;
+}
 
 int task1(FILE* input) {
     char *line = NULL;
     size_t len = 0;
-    ssize_t nread;
 
     int sum = 0;
 
-    while ((nread = getline(&line, &len, input)) != -1) {
-        char buf[len];
-        strcpy(buf, line);
-
-        char* token = strtok(buf, " ");
-        token = strtok(NULL, " ");
-        int game_num = atoi(token);
+    while (getline(&line, &len, input) != -1) {
+        char* token = strtok(line, " ");
+        int game_num = parse_game_num(token);
         bool game_success = false;
 
         token = strtok(NULL, " ");
+
         while (token) {
-            int cube_amount = atoi(token);
-            char *cube_color = strtok(NULL, " ");
+            GameIteration game = parse_game_iteration(token);
 
-            if (ends_with(cube_color, ',')
-                || ends_with(cube_color, ';')
-                || ends_with(cube_color, '\n')
-                ) {
-                cube_color[strlen(cube_color)-1] = '\0';
-            }
-
-            if (strcmp(cube_color, "red") == 0) {
-                game_success = cube_amount <= MAX_RED;
-            } else if (strcmp(cube_color, "blue") == 0) {
-                game_success = cube_amount <= MAX_BLUE;
-            } else if (strcmp(cube_color, "green") == 0) {
-                game_success = cube_amount <= MAX_GREEN;
+            if (strcmp(game.color, "red") == 0) {
+                game_success = game.amount <= MAX_RED;
+            } else if (strcmp(game.color, "blue") == 0) {
+                game_success = game.amount <= MAX_BLUE;
+            } else if (strcmp(game.color, "green") == 0) {
+                game_success = game.amount <= MAX_GREEN;
             } else {
-                printf("Unknown color %s\n", cube_color);
+                printf("Unknown color %s\n", game.color);
                 exit(1);
             }
 
@@ -69,15 +87,48 @@ int task1(FILE* input) {
         }
 
         if (game_success) {
-            printf("Game %d: %s\n", game_num, "possible");
             sum += game_num;
-        } else {
-            printf("Game %d: %s\n", game_num, "impossible");
         }
     }
 
     return sum;
+}
 
+int task2(FILE* input) {
+    char *line = NULL;
+    size_t len = 0;
+
+    int sum = 0;
+
+    while (getline(&line, &len, input) != -1) {
+        char *token = strchr(line, ':')+1;
+        token = strtok(token, " ");
+
+        int max_red = 0;
+        int max_blue = 0;
+        int max_green = 0;
+
+        while (token) {
+            GameIteration game = parse_game_iteration(token);
+
+            if (strcmp(game.color, "red") == 0) {
+                max_red = (int)fmax(max_red*1.0, game.amount*1.0);
+            } else if (strcmp(game.color, "blue") == 0) {
+                max_blue = (int)fmax(max_blue*1.0, game.amount*1.0);
+            } else if (strcmp(game.color, "green") == 0) {
+                max_green = (int)fmax(max_green, game.amount);
+            } else {
+                printf("Unknown color %s\n", game.color);
+                exit(1);
+            }
+
+            token = strtok(NULL, " ");
+        }
+
+        sum += max_red * max_blue * max_green;
+    }
+
+    return sum;
 }
 
 int main() {
@@ -85,5 +136,11 @@ int main() {
 
     int result1 = task1(input);
     printf("[task1] %d\n", result1);
+
+    fseek(input, 0, SEEK_SET);
+
+    int result2 = task2(input);
+    printf("[task2] %d\n", result2);
+
     return fclose(input);
 }
